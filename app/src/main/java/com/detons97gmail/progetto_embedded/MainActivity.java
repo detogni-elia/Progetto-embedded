7f4fe6935d252ef5ce345a408dde289d158c85de
detogni-elia <eliadetogni@protonmail.com>
2020-05-09 15:18:19 +0200

    First commit

---
+++ app/src/main/java/com/detons97gmail/progetto_embedded/MainActivity.java
@@ -0,0 +1,14 @@
+package com.detons97gmail.progetto_embedded;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
