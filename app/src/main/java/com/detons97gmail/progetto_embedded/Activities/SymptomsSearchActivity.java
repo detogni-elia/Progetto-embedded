4e9ee4e8bed9f6ec62cd5fc40fc7c5033aa48f46
detogni-elia <eliadetogni@protonmail.com>
2020-05-19 01:31:40 +0200

    Realized symptoms search and selection, implemented also search for symptom.

--- app/src/main/java/com/detons97gmail/progetto_embedded/Activities/SymptomsSearchActivity.java
+++ app/src/main/java/com/detons97gmail/progetto_embedded/Activities/SymptomsSearchActivity.java
@@ -1,14 +1,68 @@
 package com.detons97gmail.progetto_embedded.Activities;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class SymptomsSearchActivity extends AppCompatActivity {
+import com.detons97gmail.progetto_embedded.Adapters.SymptomsSearchAdapter;
+import com.detons97gmail.progetto_embedded.R;
+import com.detons97gmail.progetto_embedded.Values;
+
+import java.util.ArrayList;
+
+//TODO: USE FRAGMENT TO RETAIN ADAPTER AND CLEAN CODE
+public class SymptomsSearchActivity extends AppCompatActivity implements SymptomsSearchAdapter.ToDefineListener {
+    private String[] symptoms;
+    private SymptomsSearchAdapter adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_symptoms_search);
+        int[] symptomsIds = Values.SYMPTOMS_IDS;
+        symptoms = new String[symptomsIds.length];
+        for(int i = 0; i < symptoms.length; i++)
+            symptoms[i] = getString(symptomsIds[i]);
+
+        ArrayList<SymptomsSearchAdapter.DataWrapper> data = SymptomsSearchAdapter.DataWrapper.getWrappedData(symptoms);
+        adapter = new SymptomsSearchAdapter(data, this);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+        MenuItem searchItem = menu.getItem(0);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public void onSwitchClicked(int symptomPosition, boolean selected) {
+        Log.v("SymptomsSearchActivity", "SWITCH DI SINTOMO: " + symptoms[symptomPosition] + " ORA VALE " + selected);
+    }
 }
---
+++ app/src/main/java/com/detons97gmail/progetto_embedded/Adapters/SymptomsSearchAdapter.java
@@ -0,0 +1,143 @@
+package com.detons97gmail.progetto_embedded.Adapters;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.detons97gmail.progetto_embedded.R;
+
+import java.util.ArrayList;
+
+public class SymptomsSearchAdapter extends RecyclerView.Adapter<SymptomsSearchAdapter.SymptomsViewHolder> implements Filterable {
+    private ArrayList<DataWrapper> fullData;
+    private ArrayList<DataWrapper> filteredData = new ArrayList<>();
+    private ToDefineListener listener;
+
+    public SymptomsSearchAdapter(ArrayList<DataWrapper> data, ToDefineListener listener){
+        fullData = data;
+        filteredData.addAll(data);
+        this.listener = listener;
+    }
+
+    @Override
+    public SymptomsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RelativeLayout item = (RelativeLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.symptoms_search_item, parent, false);
+        return new SymptomsViewHolder(item, -1, listener);
+    }
+
+    @Override
+    public void onBindViewHolder(SymptomsViewHolder holder, int position) {
+        holder.symptom.setText(filteredData.get(position).symptom);
+        holder.symptomSwitch.setChecked(filteredData.get(position).selected);
+        holder.symptomAbsolutePosition = filteredData.get(position).absolutePos;
+    }
+
+    @Override
+    public int getItemCount() {
+        return filteredData.size();
+    }
+
+    @Override
+    public Filter getFilter(){
+        return filter;
+    }
+
+    private Filter filter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            ArrayList<DataWrapper> filtered = new ArrayList<>();
+            if(constraint.toString().isEmpty())
+                filtered.addAll(fullData);
+            else{
+                for(DataWrapper wrapper: fullData){
+                    String symptom = wrapper.getSymptom();
+                    if(symptom.toLowerCase().contains(constraint.toString().toLowerCase()))
+                        filtered.add(wrapper);
+                }
+            }
+            FilterResults filterResults = new FilterResults();
+            filterResults.values = filtered;
+            return filterResults;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            filteredData.clear();
+            filteredData.addAll((ArrayList<DataWrapper>)results.values);
+            notifyDataSetChanged();
+        }
+    };
+
+    public static class DataWrapper{
+        private String symptom;
+        private boolean selected;
+        private int absolutePos;
+
+        public DataWrapper(String s, boolean sel, int absolutePos){
+            symptom = s;
+            selected = sel;
+            this.absolutePos = absolutePos;
+        }
+        public void setSymptom(String s){
+            symptom = s;
+        }
+        public void setSelected(boolean sel){
+            selected = sel;
+        }
+        public void setAbsolutePos(int pos){
+            absolutePos = pos;
+        }
+        public String getSymptom(){
+            return symptom;
+        }
+        public boolean getSelected(){
+            return selected;
+        }
+        public int getAbsolutePos(){
+            return absolutePos;
+        }
+        public static ArrayList<DataWrapper> getWrappedData(String[] symptomsList){
+            ArrayList<DataWrapper> toReturn = new ArrayList<>();
+            for(String symptom: symptomsList){
+                toReturn.add(new DataWrapper(symptom, false, toReturn.size()));
+            }
+            return toReturn;
+        }
+    }
+
+    class SymptomsViewHolder extends RecyclerView.ViewHolder {
+        private TextView symptom;
+        private Switch symptomSwitch;
+        private int symptomAbsolutePosition = -1;
+        private ToDefineListener listener;
+
+        SymptomsViewHolder(RelativeLayout root, int position, ToDefineListener toDefineListener){
+            super(root);
+            symptom = root.findViewById(R.id.textView);
+            symptomSwitch = root.findViewById(R.id.symptomSwitch);
+            symptomAbsolutePosition = position;
+            listener = toDefineListener;
+            symptomSwitch.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.v("ViewHolder", "CLICCATO");
+                    boolean isChecked = symptomSwitch.isChecked();
+                    fullData.get(symptomAbsolutePosition).selected = isChecked;
+                    listener.onSwitchClicked(symptomAbsolutePosition, isChecked);
+                }
+            });
+        }
+    }
+
+    public interface ToDefineListener{
+        void onSwitchClicked(int symptomPosition, boolean selected);
+    }
+}
